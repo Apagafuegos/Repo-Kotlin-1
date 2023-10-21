019e43ea57b5bdf8a792bf3091b935a9b7c3cea4
Extinthor <csantos6952@ceeuandalucia.es>
2023-10-21 14:11:03 +0200

    Eing

---
+++ src/main/kotlin/Ejercicio 1.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+fun main() {
+    val sc = Scanner(System.`in`)
+    println("Cual es tu nombre?")
+    val nombre = sc.nextLine()
+    println("Cual es tu apellido?")
+    val apll = sc.nextLine()
+
+    println("Tu nombre completo es $nombre $apll")
+
+
+}
